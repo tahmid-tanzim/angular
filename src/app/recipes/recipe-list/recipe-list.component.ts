942d91434ec5d05cdebd44543f83527e40a9aab1
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2017-06-08 11:59:18 +0600

    Adding contents to the Recipes Component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Kacchi Biryani', 'Slow cooked chicken, rice, curd, saffron and varied masalas combined together to make a delicious and aromatic biryani.', 'http://rupcare.com/wp-content/uploads/2016/09/rupcare_kacchi-biryani-recipe.jpg'),
+    new Recipe('Beef Steak', 'A beefsteak is a flat cut of beef, usually cut perpendicular to the muscle fibers. Beefsteaks are usually grilled, pan-fried, or broiled.' ,'https://www.wish-bone.com/wp-content/uploads/2013/11/CaribbeanJerkBeefSteak.jpg')
+  ];
 
   constructor() { }
 
