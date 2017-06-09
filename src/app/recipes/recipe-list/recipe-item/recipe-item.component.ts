e5a3d828dda65fe958e7997b98cb002a170fa552
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2017-06-09 11:15:44 +0600

    Binding to Custom Properties

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: {name: string, description: string, imagePath: string};
 
   constructor() { }
 
